0ebd9ff4a62db79c5c54e2e1d7e715cf539c289a
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-05-23 13:05:23 +0530

    Navbar options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,13 +16,10 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Form</a>
+                <a className="nav-link active" aria-current="page" href="/">Word Meaning</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">{props.about}</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" aria-disabled="true" href='/'>Word Meaning</a>
+                <a className="nav-link" aria-disabled="true" href='/'>About</a>
               </li>
             </ul>
             <div className="form-check form-switch mx-5">
